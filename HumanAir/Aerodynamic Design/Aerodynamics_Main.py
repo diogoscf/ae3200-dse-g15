f31c047082b749044e847213518d2490b582aea8
Mikado-03 <m.ruyssenaars@student.tudelft.nl>
2024-05-27 17:41:33 +0200

    updates, yes

--- HumanAir/Aerodynamic Design/Aerodynamics_Main.py
+++ HumanAir/Aerodynamic Design/Aerodynamics_Main.py
@@ -1,6 +1,37 @@
 from ISA import ISA
 from PlanformDesign import Planform
+from FlowParameters import Mach, Reynolds
 
-from aircraft_data import aircraft_data
+MTOW=1946.34
+WS=618
+AR=9.35
+Taper = 0.4
+QuarterChordSweep = 0
+CruiseHeight=3000
+TemperatureGradient=-0.0065
+CruiseVelocity=60
+
+checkwingplanform=False
+checkflowparameters=False
 
 WingPlanform = Planform(MTOW, WS, AR, Taper, QuarterChordSweep)
+ISACruise = ISA(CruiseHeight, TemperatureGradient)
+
+if checkwingplanform:
+    WingPlanform.PlotWingPlanform()
+
+MACH=Mach(CruiseVelocity, ISACruise.SpeedOfSound())
+Reynolds=Reynolds(CruiseVelocity,ISACruise.DynamicViscosity(), WingPlanform.MAC(), ISACruise.Density())
+
+if checkflowparameters:
+    print("MACH = ", MACH)
+    print("Reynolds = ", Reynolds)
+
+
+
+
+
+
+
+
+
--- HumanAir/Aerodynamic Design/FlowParameters.py
+++ HumanAir/Aerodynamic Design/FlowParameters.py
@@ -1,2 +1,5 @@
-class FlowParameters:
-    def __init__(self, ):
+def Mach(V, a):
+    return V/a
+
+def Reynolds(V, Mu, MAC, Rho):
+    return V*MAC*Rho/Mu
