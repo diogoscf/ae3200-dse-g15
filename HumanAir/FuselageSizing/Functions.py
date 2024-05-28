40fe4c3e3b6227cecd2f1c730cbc76866f88c80c
eunkcho03 <118188329+eunkcho03@users.noreply.github.com>
2024-05-28 15:03:08 +0200

    update

--- HumanAir/FuselageSizing/Functions.py
+++ HumanAir/FuselageSizing/Functions.py
@@ -2,10 +2,9 @@ import numpy as np
 import re
 import pandas as pd
 # Define the path to the text file
+# file_path = 'data.txt'
 
-def import_data(file_path):
-    # file_path = 'data.txt'
-    
+def import_data(file_path):    
     # Initialise lists
     column_names = []
     data_rows = []
@@ -17,14 +16,14 @@ def import_data(file_path):
         
         header_found = False
         for line in lines:
-            if re.match(r'^\s*\S+\s\S+', line):
+            if re.match(r'^\s*alpha\s+Beta\s+CL\s+CDi\s+CDv\s+CD\s+CY\s+Cl\s+Cm\s+Cn\s+Cni\s+QInf\s+XCP\s*$', line):
                 column_names = re.findall(r'\S+', line.strip())
                 header_found = True
                 continue
-            
+
             if header_found and re.match(r'^\s*-?\d+', line):
                 data_rows.append(re.findall(r'-?\d+\.\d+', line.strip()))
-    
+                
     df = pd.DataFrame(data_rows, columns=column_names)
     df = df.astype(float)
     return df
--- HumanAir/FuselageSizing/Test.py
+++ HumanAir/FuselageSizing/Test.py
@@ -1,2 +1,6 @@
 from Functions import import_data
 
+file_path = 'HumanAir/FuselageSizing/DATA_EXAMPLE.txt'
+df = import_data(file_path)
+
+print(df)
