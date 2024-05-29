f006e1dcb83aa82c2a7a933ce56188d4e27ec44b
Diogo S.C. Fernandes <diogo@diogoscf.eu>
2024-05-29 17:00:39 +0200

    Update HumanAir/Weights_and_CG/weight_fractions.py

--- HumanAir/Weights_and_CG/weight_fractions.py
+++ HumanAir/Weights_and_CG/weight_fractions.py
@@ -55,7 +55,8 @@ def find_lg(ac_datafile = aircraft_data):
 
 def component_mass(ac_datafile = aircraft_data):
     # Import statistical weight fraction data
-    fracs = pd.read_csv('../Weights_and_CG/fraction-database.csv', index_col=0).to_numpy()
+    fracs_file= os.path.join(os.path.dirname(__file__), "fraction-database.csv.csv")
+    fracs = pd.read_csv(fracs_file, index_col=0).to_numpy()
 
     # Convert weights to kg and with contingency
     # TODO: Change to actual datafile
