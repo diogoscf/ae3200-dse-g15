d724badd99fb2d0a322538a9f1b26a85fce43f98
Alex <hoiberg@users.noreply.github.com>
2024-06-04 17:41:59 +0200

    End of day commit

--- HumanAir/Flight Performance/aircraft.py
+++ HumanAir/Flight Performance/aircraft.py
@@ -1,21 +1,39 @@
-from helper import *
+from helper import density
 import json
 import numpy as np
+from scipy.optimize import root_scalar
+#from scipy.integrate import quad
 
 class Aircraft:
     def __init__(self, FILE="design.json"):
         with open("../Configurations/" + FILE,'r') as f:
             dat = json.load(f)
             
+        # assumptions:
+        # - variable pitch propeller (in takeoff run calculation)
+            
+        # settings
+        self.CL_climb_safetyfactor = 1.2 # from ADSEE I lecture 3
+        
+        # performance
+        self.V_cruise       = dat["Performance"]["Vc_m/s"]
+        self.h_cruise       = dat["Performance"]["Altitude_Cruise_m"]
+        self.h_cruise_max   = dat["Performance"]["Altitude_Max_Cruise_m"]
+        self.dT_cruise      = dat["Performance"]["Temp_offset_TO_Land_cruise"]
+
         # aerodynamic data
-        self.CD0            = dat["Aero"]["CD0"]
+        self.CD0_clean      = dat["Aero"]["CD0"]
         self.AR             = dat["Aero"]["AR"]
-        self.e              = dat["Aero"]["e"]
-        self.S              = 19.32 # GA8
+        self.e_clean        = dat["Aero"]["e"]
+        self.S              = dat["Aero"]["S_Wing"]
         
         self.CLmax_clean    = dat["Aero"]["CLmax_clean"]
         self.CLmax_TO       = dat["Aero"]["CLmax_TO"]
-        self.CLmax_land     = dat["Aero"]["CLmax_land"]
+        self.CLmax_land     = dat["Aero"]["CLmax_TO"] # TODO: check if still correct
+        
+        #self.CL_ground      = .5 # C_L while on level runway TODO: fill in
+        self.number_of_engines = 1 # TODO: use design.json
+        self.propeller_diameter = 2 # TODO: use deisng.json
 
         # weights
         self.W_OE           = dat["Weights"]["OEW_N"]
@@ -26,52 +44,153 @@ class Aircraft:
         self.W_MTO          = dat["Weights"]["MTOW_N"]
 
         # propulsion
-        self.sealevelpower  = 220000 # GA8
+        self.max_cont_power_sealevel  = 250500 # engine power without losses TODO: correct
+        self.takeoff_power_sealevel   = 250500 # engine power without losses TODO: correct
         self.eff_powertrain = dat["Power_prop"]["eta_powertrain"] # TODO: check definition
         self.eff_prop       = dat["Power_prop"]["eta_p"]
         
         # calculated data
-        self.LDmax = 0.5 * np.sqrt(np.pi*self.AR*self.e/self.CD0) # ruijgrok p107
-        self.CD_LDmax = 2 * self.CD0 # ruijgrok p106
+        self.LDmax = 0.5 * np.sqrt(np.pi*self.AR*self.e_clean/self.CD0_clean) # ruijgrok p107
+        self.CD_LDmax = 2 * self.CD0_clean # ruijgrok p106
         self.CL_LDmax = self.CD_LDmax * self.LDmax
         
-        self.L3D2max = 3 * np.sqrt(3) / 16 * np.pi * self.AR * self.e \
-            * np.sqrt(np.pi * self.AR * self.e / self.CD0) # for P_r_min for max climbrate; ruijgrok p107
-        self.CD_L3D2max = 4 * self.CD0 # ruijgrok p107
+        self.L3D2max = 3 * np.sqrt(3) / 16 * np.pi * self.AR * self.e_clean \
+            * np.sqrt(np.pi * self.AR * self.e_clean / self.CD0_clean) # for P_r_min for max climbrate; ruijgrok p107
+        self.CD_L3D2max = 4 * self.CD0_clean # ruijgrok p107
         self.CL_L3D2max = (self.L3D2max * self.CD_L3D2max**2)**(1/3)
+                
+    def CD(self, CL, gear="up", flaps="up"): # this parabolic approximation is about accurate until CL=1 (ruijgrok p226)
+        """ Parabolic estimation of C_D for given C_L for given gear/flaps conditions (default=clean) """
         
+        # TODO: replace by more accurate representation for high-alpha CD estimation
         
-    def CD(self, CL): # this parabolic approximation is about accurate until CL=1 (ruijgrok p226)
-        """ Parabolic estimation of C_D for given C_L """
-        return self.CD0 + self.CL**2 / (np.pi * self.AR * self.e)
+        CD0 = self.CD0_clean
+        e = self.e_clean
+        
+        if gear == "down":
+            CD0 += 0.005 # TODO: estimate
+            
+        if flaps == "TO":
+            CD0 += 0.005 # TODO: estimate
+            e -= 0.05 # TODO: estimate
+        elif flaps == "land":
+            CD0 += 0.01 # TODO: estimate
+            e -= 0.1 # TODO: estimate
+            
+        return CD0 + CL**2 / (np.pi * self.AR * e)
     
     def V_Dmin(self, W, h, dT):
-        """ Calculates velocity corresponding with minimum drag """
-        a = 1 / (self.CD0 * np.pi * self.AR * self.e)
+        """ Calculates velocity corresponding with minimum drag in clean configuration """
+        a = 1 / (self.CD0_clean * np.pi * self.AR * self.e_clean)
         return np.sqrt( W/self.S * 2/density(h, dT) * a )
     
     def D_min(self, W):
-        """ Minimum drag (in Newtons) for given weight. Corresponds to V_Dmin """
+        """ Minimum drag (in Newtons) for given weight in clean configuration. Corresponds to V_Dmin """
         return W / self.LDmax
     
     def P_a(self, h, dT):
-        """ Available power for given conditions
+        """ Max continuous power for given conditions
         TODO: temporary placeholder implementation
         NOTE: RC_max() assumes constant P_a with velocity, if this changes here
         then the implementation of that function must be changed too """
         alt_correction = (density(h, dT)/1.225)**0.7
-        return alt_correction * self.eff_powertrain * self.eff_prop * self.sealevelpower
+        return alt_correction * self.eff_powertrain * self.eff_prop * self.max_cont_power_sealevel
+    
+    def P_to(self, h, dT):
+        """ Available takeoff power for given conditions
+        TODO: temporary placeholder implementation
+        NOTE: RC_max() assumes constant P_a with velocity, if this changes here
+        then the implementation of that function must be changed too """
+        alt_correction = (density(h, dT)/1.225)**0.7
+        return alt_correction * self.eff_powertrain * self.eff_prop * self.takeoff_power_sealevel
+
         
     def P_r_min(self, W, h, dT):
-        """ Minimum possible value for  power required for given conditions,
+        """ Minimum possible value for  power required for given conditions in clean configuration,
             note that this assumes a small flight path angle. """
         return W * np.sqrt(W/self.S * 2/density(h, dT) * 1/self.L3D2max) # ruijgrok p221
         
     def RC_max(self, W, h, dT):
-        """ Maximum possible climb rate for given conditions """
+        """ Maximum possible climb rate for given conditions in clean configuration"""
         return (self.P_a(h, dT) - self.P_r_min(W, h, dT)) / W
     
-    def climb_angle_max(self, W, h, dT):
-        """ Maximum possible climb angle for given conditions in % slope """
-        a = (self.P_a(h, dT)/self.V_Dmin(W, h, dT) - self.D_min(W)) / W * 100 # TODO: incorrect, assumes const thrust HIER VERDER
-        return a/np.sqrt(1-a**2) # since it equals sine(gamma) this converts it to % slope
+    def climb_angle_max(self, W, h, dT, gear="up", flaps="up"):
+        """ Maximum possible climb slope (RC/V) for given conditions in given config (default clean) """
+        # adsee I q3 lecture 3 slide 93
+        if flaps == "up":
+            CL = self.CLmax_clean / self.CL_climb_safetyfactor
+        elif flaps == "TO":
+            CL = self.CLmax_TO / self.CL_climb_safetyfactor
+        elif flaps == "land":
+            CL = self.CLmax_land / self.CL_climb_safetyfactor
+        return self.P_a(h, dT)/W * 1/np.sqrt(W/self.S * 2/density(h, dT) * 1/CL) - self.CD(CL, gear=gear, flaps=flaps)/CL
+    
+    def stall_speed(self, W, h, dT, flaps="up"):
+        """ Calculates stall speed for given conditions in given config (default clean) """
+        if flaps == "up":
+            CL = self.CLmax_clean
+        elif flaps == "TO":
+            CL = self.CLmax_TO
+        elif flaps == "land":
+            CL = self.CLmax_land
+        return np.sqrt(W / (0.5 * density(h, dT) * self.S * CL))
+    
+    def V_max(self, W, h, dT):
+        """ Calculates the maximum true airspeed for given conditions in clean config """
+        P_a = self.P_a(h, dT)
+        rho = density(h, dT)
+        def diff(V):
+            CL = W / (0.5 * rho * self.S * V**2)
+            CD = self.CD(CL)
+            D = 0.5 * rho * self.S * CD * V**2
+            return P_a - D*V
+        return root_scalar(diff, x0=75).root
+    
+    def power_setting(self, W, V, h, dT):
+        """ Gives power setting required to maintain cruise (0-1, but also goes beyond 1) """
+        CL = W / (0.5 * density(h, dT) * self.S * V**2)
+        D = 0.5 * density(h, dT) * self.S * self.CD(CL) * V**2
+        if CL > 1: return 0
+        return V * D / self.P_a(h, dT)
+
+    def takeoff_ground_run(self, W, h, dT, slope):
+
+        # this uses the torenbeek method, torenbeek p167-168
+        # however it really is not accurate enough
+        
+        # also ground effect should be taken into account
+        
+        rho = density(h, dT)
+        sigma = rho/1.225
+        g = 9.80665
+        
+        CLmax = self.CLmax_TO
+        V_S_TO = np.sqrt(W / (0.5 * rho * self.S * CLmax)) # TODO: ground effect should be taken into account here
+        V_LOF = 1.2 * V_S_TO # lift off speed approximation according to torenbeek
+        #CL_LOF = W / (0.5 * rho * V_LOF**2 * self.S)
+        CD0 = self.CD(0, gear="down", flaps="TO")
+        
+        P_to = self.P_to(h, dT) / g # in kgm/s
+        
+        mu_quote = 0.05 + .72 * CD0/CLmax # 0.04-0.05 for short grass and 0.02 for concrete, if a fixed pitch prop is used use 0.576
+        
+        # avg thrust in kgm/s
+        Tbar = .321 * P_to * (sigma / self.number_of_engines * self.propeller_diameter**2 / P_to)**(1/3)
+        Tbar *= g # convert to W
+        
+        s_run = V_LOF**2 / (2*g) / (Tbar/W - mu_quote)
+        
+        return s_run
+    
+        #CL_G = self.CL_ground
+        #CD_G = self.CD(CL_G)
+        
+        #def s_g(V):
+           # return V / (g * (P_a(h, dT)))
+        
+        
+        #mu_r = 0.05 # coefficient of rolling friction for short-cut grass, ruijgrok p372 and roskam pt7 p40
+        
+        
+        
+        
--- HumanAir/Flight Performance/climb-performance.py
+++ HumanAir/Flight Performance/climb-performance.py
@@ -5,10 +5,10 @@ from pprint import pprint
 import helper
 import aircraft
 
+h_max = 9900
 
 def plot_climb_rate(acf):
-    h_max = 7000
-    
+   
     W = acf.W_MTO
     dT = 0    
     
@@ -24,13 +24,14 @@ def plot_climb_rate(acf):
         P_a[i] = acf.P_a(h, dT)
         P_r_min[i] = acf.P_r_min(W, h, dT)
 
-        # service ceiling defined as altitude with 100ft/min (0.508m/s) max climbrate
-        # FAA Pilots Handbook of Aeronautical Knowledge, but it is not a fixed definition I think
+        # service ceiling commonly defined as altitude with 100ft/min (0.508m/s) max climbrate
+        # FAA Pilots Handbook of Aeronautical Knowledge
+        # also Roskam pt 7 p129
         if RC_max[i] < 0.508 and service_ceiling < 0.01 and i > 0:
             service_ceiling = h_list[i-1]
     
-    print(f"Sea level MTOW max climb rate: {RC_max[0]:.3f} m/s")
-    print(f"Service ceiling: {service_ceiling:.0f} m")
+    print(f"Sea level MTOW max climb rate, clean config: {RC_max[0]:.3f} m/s")
+    print(f"Service ceiling, MTOW, clean config: {service_ceiling:.0f} m")
     
     plt.figure(figsize=(10,7))
     plt.plot(RC_max, h_list, label="Maximum Climb Rate")
@@ -42,45 +43,139 @@ def plot_climb_rate(acf):
     plt.ylim(0,h_max)
     plt.xlim(0,RC_max[0]*1.05)
     plt.legend()
+    plt.text(0, h_max+100, "Not accurate for high altitudes with C_L > 1")
     #plt.subplots_adjust(right=0.75)
     #plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
-    plt.savefig("climb_rate.svg")
+    plt.savefig("plots/climb_rate.svg")
     plt.show()
     
 def plot_climb_gradient(acf):
-    h_max = 7000
     
     W = acf.W_MTO
     dT = 0    
     
     h_list = np.arange(0, h_max, 10)
-    max_gradient = np.zeros(len(h_list))
-    T = np.zeros(len(h_list))
-    D = np.zeros(len(h_list))
+    max_gradient_clean = np.zeros(len(h_list))
+    max_gradient_TO = np.zeros(len(h_list))
+    max_gradient_land = np.zeros(len(h_list))
+
     
     for i, h in enumerate(h_list):
-        max_gradient[i] = acf.climb_angle_max(W, h, dT)
-        T[i] = acf.P_a(h, dT)/acf.V_Dmin(W, h, dT)
-        D[i] = acf.D_min(W)
+        max_gradient_clean[i] = acf.climb_angle_max(W, h, dT) * 100
+        max_gradient_TO[i] = acf.climb_angle_max(W, h, dT, gear="up", flaps="TO") * 100
+        max_gradient_land[i] = acf.climb_angle_max(W, h, dT, gear="down", flaps="land") * 100
 
-    print(f"Sea level MTOW max climb gradient: {max_gradient[0]:.3f} %")
-    
+    print(f"Sea level MTOW max climb gradient, clean config: {max_gradient_clean[0]:.3f} %")
+    print(f"Sea level MTOW max climb gradient, gear up, take-off flaps: {max_gradient_TO[0]:.3f} %")
+    print(f"Sea level MTOW max climb gradient, gear down, landing flaps: {max_gradient_land[0]:.3f} %")
+
     plt.figure(figsize=(10,7))
-    plt.plot(max_gradient, h_list, label="Maximum Climb Gradient")
-    plt.plot(T/1000, h_list, color="g", label="Thrust /1000")
-    plt.plot(D/1000, h_list, color="r", label="Drag /1000")
-    plt.xlabel(r"Climb Gradient (%)")
+    plt.plot(max_gradient_clean, h_list, color="b", label="MTOW, gear up, flaps up")
+    plt.plot(max_gradient_TO, h_list, color="purple", label="MTOW, gear up, take-off flaps")
+    plt.plot(max_gradient_land, h_list, color="lightblue", label="MTOW, gear up, landing flaps")
+    plt.xlabel(r"Maximum Climb Gradient (%)")
     plt.ylabel("Altitude (m)")
     plt.ylim(0,h_max)
-    plt.xlim(0,max_gradient[0]*1.05)
+    plt.xlim(0,max(max_gradient_clean[0], max_gradient_TO[0], max_gradient_land[0])*1.05)
     plt.legend()
+    plt.text(0, h_max+100, "Not accurate currently due to inaccurate C_D approximation for high C_L")
+
     #plt.subplots_adjust(right=0.75)
     #plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
-    plt.savefig("climb_gradient.svg")
+    plt.savefig("plots/climb_gradient.svg")
     plt.show()
+    
+def plot_stall_speed(acf):
+    
+    W = acf.W_MTO
+    dT = 0    
+    
+    h_list = np.arange(0, h_max, 10)
+    stall_clean = np.zeros(len(h_list))
+    stall_TO = np.zeros(len(h_list))
+    stall_land = np.zeros(len(h_list))
+    V_max = np.zeros(len(h_list))
+    
+    stop_V_max = False
+    last_V_max = (0,0)
+    
+    for i, h in enumerate(h_list):
+        stall_clean[i] = acf.stall_speed(W, h, dT)
+        stall_TO[i] = acf.stall_speed(W, h, dT, flaps="TO")
+        stall_land[i] = acf.stall_speed(W, h, dT, flaps="land")
+        
+        if not stop_V_max:
+            V_max[i] = acf.V_max(W, h, dT) # after max speed crosses stall speed graph gets weird
+            if V_max[i] < stall_clean[i]:
+                stop_V_max = True
+                last_V_max = ([V_max[i-1],V_max[i]], [h_list[i-1],h_list[i]])
+                V_max[i] = 0
+        else:
+            V_max[i] = 0
+
+
+    print(f"Sea level MTOW stall speed, clean config: {stall_clean[0]:.3f} m/s")
+    print(f"Sea level MTOW stall speed, take-off flaps: {stall_TO[0]:.3f} m/s")
+    print(f"Sea level MTOW stall speed, landing flaps: {stall_land[0]:.3f} m/s")
+    print(f"Sea level MTOW maximum speed, clean config: {V_max[0]:.3f} m/s")
+
+    plt.figure(figsize=(10,7))
+    plt.plot(stall_clean, h_list, color="b", label="Stall, MTOW, flaps up")
+    plt.plot(stall_TO, h_list, color="purple", label="Stall, MTOW, take-off flaps")
+    plt.plot(stall_land, h_list, color="lightblue", label="Stall, MTOW, landing flaps")
+    plt.plot(V_max, h_list, color="red", label="Maximum velocity, MTOW, clean configuration")
+    plt.scatter(last_V_max[0], last_V_max[1], s=100, color="red", label="Last V_max before crossing stall speed")
+    plt.xlabel("Velocity (m/s)")
+    plt.ylabel("Altitude (m)")
+    plt.ylim(0,h_max)
+    plt.xlim(0, 70)
+   #plt.xlim(0,max(stall_clean[0], stall_TO[0], stall_land[0])*1.05)
+    plt.legend()
+    
+    plt.text(0, h_max+100, "V_max not accurate for high altitudes with C_L > 1")
+
+    #plt.subplots_adjust(right=0.75)
+    #plt.legend(bbox_to_anchor=(1.01, 1), loc="upper left")
+    plt.savefig("plots/stall_speed.svg")
+    plt.show()
+    
+def plot_cruise_power_setting(acf):
+    
+    W = acf.W_MTO
+    dT = 0    
+    
+    h_list = np.arange(0, h_max, 10)
+    powersetting = np.zeros(len(h_list))
+    
+    for i, h in enumerate(h_list):
+        powersetting[i] = acf.power_setting(W, acf.V_cruise, h, dT)
+        
+        
+    std_cruise = acf.power_setting(W, acf.V_cruise, acf.h_cruise, acf.dT_cruise)
+    max_cruise = acf.power_setting(W, acf.V_cruise, acf.h_cruise_max, acf.dT_cruise)
+    print(f"Power setting at MTOW, V={acf.V_cruise}, h={acf.h_cruise} ISA+{acf.dT_cruise}: {std_cruise:.3f}")
+    print(f"Power setting at MTOW, V={acf.V_cruise}, h={acf.h_cruise_max} ISA+{acf.dT_cruise}: {max_cruise:.3f}")
+
+
+    plt.figure(figsize=(10,7))
+    plt.plot(powersetting, h_list, color="b", label=f"MTOW, {acf.V_cruise:.1f} m/s")
+    plt.xlabel("Power setting (-)")
+    plt.ylabel("Altitude (m)")
+    plt.ylim(0,h_max)
+    plt.xlim(powersetting[0]*0.9, 1.0)
+    plt.legend()
+    plt.text(powersetting[0]*0.9, h_max+100, "Not accurate for high altitudes with C_L > 1")
+    plt.savefig("plots/cruise_power_setting.svg")
+    plt.show()
+    
+def plot_take_off_performance(acf):
+    print(acf.takeoff_ground_run(acf.W_MTO, 0, 0, 0))
 
 if __name__ == "__main__":
     acf = aircraft.Aircraft()
     plot_climb_rate(acf)
     plot_climb_gradient(acf)
+    plot_stall_speed(acf)
+    plot_cruise_power_setting(acf)
+    plot_take_off_performance(acf)
     
