f94cc9cf8c9c048e200c03d67e7ec828fce1ba27
CristianPetria <M.C.Petria@student.tudelft.nl>
2024-06-07 08:54:11 +0200

    Merged the position function into this branch

--- HumanAir/FuselageSizing/FuselageSizing.py
+++ HumanAir/FuselageSizing/FuselageSizing.py
@@ -5,7 +5,7 @@ import matplotlib.patches as patches
 import os
 import sys
 import time
-#position update
+#position update v1
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from HumanAir.aircraft_data import aircraft_data
